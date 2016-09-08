7cabc236cd66709f56ac6bf99e7638d2740d0289
JudgeSchnoopy <jeffreytgolden@gmail.com>
2016-09-08 11:16:03 -0500

    this too

--- atminitials.ps1
+++ atminitials.ps1
@@ -1,5 +1,6 @@
 import-module activedirectory
 
+$users = Get-ADUser -Properties info -Searchbase "ou=users,ou=Wheaton,dc=contoso,dc=com" -filter *
 ForEach($user in $users){
 	$old_info = $user.info
 	Set-ADUser -Identity $user -Replace @{info="$old_info, DTM, TMA, TMN"}
--- branchshortcutstartup.ps1
+++ branchshortcutstartup.ps1
@@ -2,3 +2,4 @@ $goodLink="c:\users\public\desktop\branch suite live.lnk"
 
 If (Test-Path $goodlink){
 Return}
+Copy-Item "\\contoso01\data\telleradv\Branch Suite Live.lnk" "c:\users\public\desktop\Branch Suite Live.lnk"
